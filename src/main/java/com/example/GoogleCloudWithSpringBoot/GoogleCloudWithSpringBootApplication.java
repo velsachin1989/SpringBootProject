eef013ec1dab8e09c1f960335cd61d75cb3f0ba4
velsachin1989 <velmurugan.be.mech@gmail.com>
2019-09-18 16:13:15 +0530

    Commit

---
+++ src/main/java/com/example/GoogleCloudWithSpringBoot/GoogleCloudWithSpringBootApplication.java
@@ -0,0 +1,22 @@
+package com.example.GoogleCloudWithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GoogleCloudWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudWithSpringBootApplication.class, args);
+	}
+	
+	@RequestMapping(value="/firstApp")
+	public String firstApp() {
+		
+		return "Hello this is GCP with springboot application";
+	}
+
+}
